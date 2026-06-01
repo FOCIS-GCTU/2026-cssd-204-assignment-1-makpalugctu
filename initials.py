1a70fec7099417705b0eacf979e577f80be3acdb
makpalugctu <2425403407@live.gctu.edu.gh>
2026-06-01 22:53:36 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,28 @@
+# File: MSA.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Martin Selasie Akpalu
+# STUDENT ID: 2425403407
+# Email: 2425403407@live.gctu.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor, Martin Akpalu, this programming assignment is my own work
+# and I have not provided this code to any other student.
 
 def main():
-  pass #code goes here
 
-
+    print()
+    print("...MSA")
+    print()
+    print("...MMMM....MMMM.........SSSSSSSSSS.........AAAAAAAAAAAA.....")
+    print("...MMMM....MMMM........SSSSSSSSSSS.........AAAAAAAAAAAA.....")
+    print("...MM.MM..MM.MM.......SSSS.................AAAA....AAAA.....")
+    print("...MM.MM..MM.MM.......SSSS.................AAAA....AAAA.....")
+    print("...MM.MM..MM.MM.........SSSSSS.............AAAAAAAAAAAA.....")
+    print("...MM..MMMM..MM...........SSSSSS...........AAAAAAAAAAAA.....")
+    print("...MM..MMMM..MM...............SSSS.........AAAA....AAAA.....")
+    print("...MM...MM...MM...**..........SSSS....**...AAAA....AAAA...**")
+    print("...MM...MM...MM...**...SSSSSSSSSS.....**...AAAA....AAAA...**")
 
 main()
